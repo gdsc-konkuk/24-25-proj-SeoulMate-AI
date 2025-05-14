79fc0fa198bb863288cb6a5a31832758f8d281ee
sjin <sjin@konkuk.ac.kr>
2025-05-14 13:49:51 +0900

    fix: add print flash

--- graph_rag_recommender/routes.py
+++ graph_rag_recommender/routes.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, Blueprint, jsonify
 from graph.create_graph import update_user_node, connect_driver
 from recommend.recommender import get_top_places_for_user
+import sys
 
 recommender = Blueprint("recommender", __name__)
 
@@ -13,7 +14,8 @@ def recommend():
     user_lat = data["x"]
     user_long = data["y"]
 
-    print(data)
+    print(data, flush=True)
+    sys.stdout.flush()
     
     driver = connect_driver()
     update_user_node(driver, user_id = user_id, liked_place_ids = liked_place_ids, styles=styles)
@@ -21,6 +23,7 @@ def recommend():
     
     # AI 추천 함수 호출
     recommended_places = get_top_places_for_user(user_id=user_id, liked_place_ids=liked_place_ids, styles=styles, user_lat=user_lat, user_long=user_long, top_k=5)
-    print("Response: ", recommended_places)
+    print("Response: ", recommended_places, flush=True)
+    sys.stdout.flush()
     
     return jsonify(recommended_places)
